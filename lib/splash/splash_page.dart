4fa61f63f857082d3a4e85e2a02db575da297efe
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-03-09 23:33:31 +0300

    Update splash page
    * Loading indicator added.
    * OEPP title added.

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:oepp/core/core.dart';
 import 'package:oepp/login/login.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
+    return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          "OEPP is Loading...",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue, fontSize: 36),
+        ),
+        LoadingIndicator()
+      ],
+    ));
   }
 }
