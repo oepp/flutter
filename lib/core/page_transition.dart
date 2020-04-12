a4827fd6cabbb1bc9f1c72a6a162b27694aaa3ab
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-04-12 16:01:33 +0300

    Add page transition

---
+++ lib/core/page_transition.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class PageTransition extends PageRouteBuilder {
+  final Widget widget;
+
+  PageTransition({this.widget})
+      : super(
+          pageBuilder: (BuildContext context, Animation<double> animation,
+              Animation<double> secondaryAnimation) {
+            return widget;
+          },
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+        );
+}
--- lib/login/login_form.dart
+++ lib/login/login_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:oepp/authentication/authentication_bloc.dart';
 import 'package:oepp/authentication/authentication_event.dart';
+import 'package:oepp/core/page_transition.dart';
 import 'package:oepp/register/register.dart';
 
 class LoginForm extends StatelessWidget {
@@ -19,39 +20,32 @@ class LoginForm extends StatelessWidget {
                 Container(
                   child: TextField(
                     controller: _usernameController,
-                    decoration: InputDecoration(
-                        labelText: 'Username'
-                    ),
+                    decoration: InputDecoration(labelText: 'Username'),
                   ),
                 ),
                 Container(
                   child: TextField(
                     controller: _passwordController,
-                    decoration: InputDecoration(
-                        labelText: 'Password'
-                    ),
+                    decoration: InputDecoration(labelText: 'Password'),
                     obscureText: true,
                   ),
                 ),
-          RaisedButton(
-            child: Text('Login'),
-            onPressed: () {
-              BlocProvider.of<AuthenticationBloc>(context).add(OnLogin());
-            },
-          ),
+                RaisedButton(
+                  child: Text('Login'),
+                  onPressed: () {
+                    BlocProvider.of<AuthenticationBloc>(context).add(OnLogin());
+                  },
+                ),
                 RaisedButton(
                   child: Text('Register'),
                   onPressed: () {
                     Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => RegisterPage()),
-                    );
+                        context, PageTransition(widget: RegisterPage()));
                   },
                 )
               ],
             ),
-          )
-      ),
+          )),
     );
   }
 }
