fd4a4e07bb537358ffbc09f0966de270256c6df0
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-04-14 00:02:32 +0300

    Add GameService

---
+++ lib/services/game_service.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+import 'package:oepp/models/game_info.dart';
+import 'package:oepp/models/game_type.dart';
+
+class GameService {
+  Future<List<GameInfo>> getPopularGames() async {
+    await Future.delayed(Duration(seconds: 1));
+    var list = new List();
+    list.add(new GameInfo(10, GameType.flashCard, "Dart 101"));
+    list.add(new GameInfo(10, GameType.fillInBlanks, "C# 101"));
+    list.add(new GameInfo(10, GameType.flashCard, "Java 101"));
+    return list;
+  }
+}
