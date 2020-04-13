13ab1ee518a6ad4eb3346bb79bbaf9f558af5b9b
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-04-14 01:12:00 +0300

    Update home_page.dart

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,25 +1,121 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:oepp/authentication/authentication_bloc.dart';
-import 'package:oepp/authentication/authentication_event.dart';
+import 'package:oepp/core/color_palette.dart';
+import 'package:oepp/models/game_info.dart';
+import 'package:oepp/services/game_service.dart';
 
 class HomePage extends StatelessWidget {
-  @override
+  final GameService _gameService = new GameService();
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Container(
-        child: Center(
-            child: RaisedButton(
-              child: Text('logout'),
-              onPressed: () {
-                BlocProvider.of<AuthenticationBloc>(context).add(OnLogout());
-              },
-            )),
-      ),
+    return FutureBuilder<List<GameInfo>>(
+      future: _gameService.getPopularGames(),
+      // a previously-obtained Future<String> or null
+      builder: (BuildContext context, AsyncSnapshot<List<GameInfo>> snapshot) {
+        List<Widget> children;
+
+        if (snapshot.hasData) {
+          children = <Widget>[
+            Container(
+                width: double.infinity,
+                height: 100.0,
+                color: ColorPalette.amethyst,
+                child: Center(
+                    child: Text(
+                  "Popular Games",
+                  style: TextStyle(fontSize: 36, color: ColorPalette.clouds),
+                ))),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+                child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              children: snapshot.data
+                  .map((item) => Card(
+                        color: Colors.transparent,
+                        elevation: 0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: ColorPalette.alizarin),
+                          child: Transform.translate(
+                            offset: Offset(50, -50),
+                            child: Container(
+                              margin: EdgeInsets.symmetric(
+                                  horizontal: 65, vertical: 63),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white),
+                              child: Icon(
+                                Icons.bookmark_border,
+                                size: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ))
+          ];
+        } else if (snapshot.hasError) {
+          children = <Widget>[
+            Icon(
+              Icons.error_outline,
+              color: Colors.red,
+              size: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Error: ${snapshot.error}'),
+            )
+          ];
+        } else {
+          children = <Widget>[
+            SizedBox(
+              child: CircularProgressIndicator(),
+              width: 60,
+              height: 60,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Text('Awaiting result...'),
+            )
+          ];
+        }
+        return Scaffold(
+          backgroundColor: Colors.grey[600],
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: Icon(Icons.menu),
+            title: Text("Home"),
+            actions: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Container(
+                  width: 36,
+                  height: 30,
+                  decoration: BoxDecoration(
+                      color: Colors.grey[800],
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(child: Text("0")),
+                ),
+              )
+            ],
+          ),
+          body: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: children,
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
