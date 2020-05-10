f7a2920d286f0047bb067ce1fe74f1e98ca255d7
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-05-10 23:34:00 +0300

    Create oepp_logo.dart

---
+++ lib/widgets/oepp_logo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:oepp/core/color_palette.dart';
+
+class OeppLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 200.0,
+        height: 200.0,
+        color: ColorPalette.amethyst);
+  }
+}
