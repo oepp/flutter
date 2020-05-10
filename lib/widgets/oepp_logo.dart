75b67b3a38c84ac3eff51c1e2c4da924bf4ee75d
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-05-11 00:29:12 +0300

    Add oepp logo

--- lib/widgets/oepp_logo.dart
+++ lib/widgets/oepp_logo.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/cupertino.dart';
-import 'package:oepp/core/color_palette.dart';
 
 class OeppLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 200.0,
-        height: 200.0,
-        color: ColorPalette.amethyst);
+    return Image(image: AssetImage('assets/images/oepp_logo.png'));
   }
 }
