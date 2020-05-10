e05bd3258da2762e86dc21ecee48783d5c235d9f
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-05-10 23:33:56 +0300

    Create form_space.dart

---
+++ lib/widgets/form_space.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class FormSpace extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+    );
+  }
+}
