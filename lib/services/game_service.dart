0ddf96405a7ec3343f5e71fda724fa916fab6a2a
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-05-11 02:39:45 +0300

    Update game_info model

--- lib/services/game_service.dart
+++ lib/services/game_service.dart
@@ -6,13 +6,6 @@ class GameService {
   Future<List<GameInfo>> getPopularGames() async {
     await Future.delayed(Duration(seconds: 1));
     var list = new List<GameInfo>();
-    list.add(new GameInfo(1, GameType.flashCard, "Dart 101"));
-    list.add(new GameInfo(1, GameType.fillInBlanks, "C# 101"));
-    list.add(new GameInfo(2, GameType.flashCard, "Java 101"));
-    list.add(new GameInfo(3, GameType.flashCard, "Python 101"));
-    list.add(new GameInfo(2, GameType.fillInBlanks, "C++ 101"));
-    list.add(new GameInfo(4, GameType.flashCard, "Unity 101"));
-    list.add(new GameInfo(3, GameType.fillInBlanks, "Dart 101"));
     return list;
   }
 }
