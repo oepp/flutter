fa09c4a03b8dcc22c7c217b4b94aef2206398595
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-05-10 23:59:52 +0300

    Update login_page.dart
    * Widgets changed with reusable form widgets.

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:oepp/core/core.dart';
 import 'package:oepp/home/home_page.dart';
 import 'package:oepp/pages/register_page.dart';
+import 'package:oepp/widgets/form_button.dart';
+import 'package:oepp/widgets/form_ink_well.dart';
+import 'package:oepp/widgets/form_input_container.dart';
+import 'package:oepp/widgets/form_input_field.dart';
+import 'package:oepp/widgets/form_space.dart';
+import 'package:oepp/widgets/oepp_logo.dart';
 
 class LoginPage extends StatelessWidget {
   final _usernameController = TextEditingController();
@@ -17,119 +23,29 @@ class LoginPage extends StatelessWidget {
                     padding: EdgeInsets.symmetric(horizontal: 40),
                     child: Column(
                       children: <Widget>[
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Container(
-                            width: 200.0,
-                            height: 200.0,
-                            color: ColorPalette.amethyst),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: ColorPalette.clouds,
-                                  blurRadius: 20,
-                                  offset: Offset(0, 10),
-                                )
-                              ]),
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                    border: Border(
-                                        bottom: BorderSide(
-                                            color: ColorPalette.clouds))),
-                                child: TextField(
-                                  controller: _usernameController,
-                                  cursorColor: ColorPalette.amethyst,
-                                  style: TextStyle(
-                                    color: ColorPalette.wetAsphalt,
-                                  ),
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: "Username",
-                                      hintStyle: TextStyle(
-                                          color: ColorPalette.silver)),
-                                ),
-                              ),
-                              Container(
-                                  padding: EdgeInsets.all(10),
-                                  child: TextField(
-                                    controller: _passwordController,
-                                    cursorColor: ColorPalette.amethyst,
-                                    style: TextStyle(
-                                      color: ColorPalette.wetAsphalt,
-                                    ),
-                                    obscureText: true,
-                                    decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: "Password",
-                                      hintStyle:
-                                          TextStyle(color: ColorPalette.silver),
-                                    ),
-                                  ))
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(context,
-                                PageTransition(widget: RegisterPage()));
-                          },
-                          highlightColor: ColorPalette.transparent,
-                          splashColor: ColorPalette.clouds,
-                          child: Text("Forgot Password?",
-                              style: TextStyle(
-                                color: ColorPalette.amethyst,
-                              )),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Container(
-                          height: 50,
-                          width: double.infinity,
-                          child: RaisedButton(
-                            shape: new RoundedRectangleBorder(
-                                borderRadius: new BorderRadius.circular(10.0),
-                                side: BorderSide(color: ColorPalette.amethyst)),
-                            onPressed: () {
-                              Navigator.push(context,
-                                  PageTransition(widget: HomePage()));
-                            },
-                            color: ColorPalette.amethyst,
-                            textColor: ColorPalette.clouds,
-                            child: Text("Login"),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(context,
-                                PageTransition(widget: RegisterPage()));
-                          },
-                          highlightColor: ColorPalette.transparent,
-                          splashColor: ColorPalette.clouds,
-                          child: Text("Create Account",
-                              style: TextStyle(
-                                color: ColorPalette.amethyst,
-                              )),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        )
+                        FormSpace(),
+                        OeppLogo(),
+                        FormSpace(),
+                        FormInputContainer(<Widget>[
+                          FormInputField(_usernameController, "Username", false),
+                          FormInputField(_passwordController, "Password", true)
+                        ]),
+                        FormSpace(),
+                        FormInkWell("Forgot Password?", () {
+                          Navigator.push(context,
+                              PageTransition(widget: RegisterPage()));
+                        }),
+                        FormSpace(),
+                        FormButton("Login", () {
+                          Navigator.push(context,
+                              PageTransition(widget: HomePage()));
+                        }),
+                        FormSpace(),
+                        FormInkWell("Create Account", () {
+                          Navigator.push(context,
+                              PageTransition(widget: RegisterPage()));
+                        }),
+                        FormSpace()
                       ],
                     )))));
   }
