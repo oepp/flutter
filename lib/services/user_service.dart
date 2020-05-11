a4e4875d72846614b5c53a0e6f88f1ef6d68e3a4
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-05-11 07:22:19 +0300

    Update user_service.dart
    * register future added.

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,9 +1,17 @@
 import 'dart:async';
+import 'package:http/http.dart' as http;
 
 class UserService {
-  Future<String> authenticate() async {
-    await Future.delayed(Duration(seconds: 1));
-    return 'token';
+  Future<http.Response> register(String username, String email, String password) async {
+    Map<String, dynamic> post = new Map<String, dynamic>();
+    post["username"] = username;
+    post["email"] = username;
+    post["password"] = password;
+    const String url = "http://localhost:3001/user/register";
+    var response = await http.post(url, body: post);
+    print('Response status: ${response.statusCode}');
+
+    return response;
   }
 
   Future<void> deleteToken() async {
