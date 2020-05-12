fe3b453577397dc220c4885a3cd6d039e13acee6
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-05-12 23:28:10 +0300

    Add AlertUtility

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:oepp/pages/splash_page.dart';
 import 'package:oepp/services/game_service.dart';
 import 'package:oepp/services/user_service.dart';
 
-GetIt getIt = GetIt.instance;
-
 void main(){
-  getIt.registerSingleton<UserService>(UserService());
-  getIt.registerSingleton<GameService>(GameService());
+  GetIt.instance.registerSingleton<UserService>(UserService());
+  GetIt.instance.registerSingleton<GameService>(GameService());
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.light
+  ));
+
   runApp(App());
 }
 
