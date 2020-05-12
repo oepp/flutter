a552aa6ebed11ad6c32696e7ed45654303149da1
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-05-12 23:55:50 +0300

    Update user_service.dart
    * New requests added.

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class UserService {
   static const url =
-      "http://192.168.1.101:3001/user/register";
+      "http://192.168.1.101:3001/user/";
   static const headers = {
     'Content-Type': 'application/json'
   };
@@ -18,23 +18,42 @@ class UserService {
       "password": password
     };
 
-    var response = await http.post(url, headers: headers, body: json.encode(body()));
+    var response = await http.post(url + "register", headers: headers, body: json.encode(body()));
 
     return (jsonDecode(response.body)["status"] == "success");
   }
 
-  Future<void> deleteToken() async {
-    await Future.delayed(Duration(seconds: 1));
-    return;
+  Future<bool> login(String username, String password) async {
+    Map<String, dynamic> body() => {
+      "username": username,
+      "password": password
+    };
+
+    var response = await http.post(url + "login", headers: headers, body: json.encode(body()));
+    print(response.body);
+
+    return (jsonDecode(response.body)["status"] == "success");
   }
 
-  Future<void> persistToken(String token) async {
-    await Future.delayed(Duration(seconds: 1));
-    return;
+  Future<bool> forgotPassword(String email) async {
+    Map<String, dynamic> body() => {
+      "email": email
+    };
+
+    var response = await http.post(url + "forgot/password", headers: headers, body: json.encode(body()));
+
+    return (jsonDecode(response.body)["status"] == "success");
   }
 
-  Future<bool> hasToken() async {
-    await Future.delayed(Duration(seconds: 1));
-    return false;
+  Future<bool> confirmPassword(String resetCode, String password, String confirmpassword) async {
+    Map<String, dynamic> body() => {
+      "resetCode": resetCode,
+      "password": password,
+      "confirmpassword": confirmpassword
+    };
+
+    var response = await http.post(url + "forgot/confirm/password", headers: headers, body: json.encode(body()));
+
+    return (jsonDecode(response.body)["status"] == "success");
   }
 }
