6df188f0b21632b89f188c7694e472966ebedc79
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-05-15 05:29:24 +0300

    Update game_info_page.dart
    * Code format fixed.

--- lib/pages/game_info_page.dart
+++ lib/pages/game_info_page.dart
@@ -2,9 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:oepp/models/game_info.dart';
-import 'package:oepp/services/user_service.dart';
+import 'package:oepp/pages/game_page.dart';
+import 'package:oepp/services/game_service.dart';
 import 'package:oepp/utilities/color_palette.dart';
-import 'package:oepp/widgets/form_button.dart';
+import 'package:oepp/utilities/page_transition.dart';
 import 'package:oepp/widgets/play_button.dart';
 
 class GameInfoPage extends StatelessWidget {
@@ -25,39 +26,47 @@ class GameInfoPage extends StatelessWidget {
             height: 200,
             child: Image.memory(_gameInfo.imageData, fit: BoxFit.scaleDown),
           ),
-          Expanded(child: Container(
+          Expanded(
+              child: Container(
             width: double.infinity,
             color: ColorPalette.greenSea,
             child: Column(
               children: <Widget>[
                 Container(
                   margin: EdgeInsets.all(20),
-                  child: Text(_gameInfo.title, style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: ColorPalette.clouds
-                  ), textAlign: TextAlign.center),
+                  child: Text(_gameInfo.title,
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: ColorPalette.clouds),
+                      textAlign: TextAlign.center),
                 ),
-                Expanded(child: Container(
-                    width: double.infinity,
-                    margin: EdgeInsets.all(20),
-                    child: Text(_gameInfo.description, style: TextStyle(
-                        fontSize: 16,
-                        color: ColorPalette.clouds
-                    ),
-                    ))),
+                Expanded(
+                    child: Container(
+                        width: double.infinity,
+                        margin: EdgeInsets.all(20),
+                        child: Text(
+                          _gameInfo.description,
+                          style: TextStyle(
+                              fontSize: 16, color: ColorPalette.clouds),
+                        ))),
                 Container(
-                    width: double.infinity,
+                  width: double.infinity,
                   margin: EdgeInsets.all(10),
-                  child: Text("Created by ${_gameInfo.creator} on ${_gameInfo.creationTime.substring(0, 7)}", style: TextStyle(
-                      fontSize: 12,
-                      color: ColorPalette.clouds
-                  ), textAlign: TextAlign.right),
+                  child: Text(
+                      "Created by ${_gameInfo.creator} on ${_gameInfo.creationTime.substring(0, 7)}",
+                      style:
+                          TextStyle(fontSize: 12, color: ColorPalette.clouds),
+                      textAlign: TextAlign.right),
                 )
               ],
             ),
           )),
-          PlayButton(()=>{})
+          PlayButton(() {
+            GetIt.instance<GameService>().getGame(_gameInfo.id).then((game) {
+              Navigator.push(context, PageTransition(widget: GamePage(game)));
+            });
+          })
         ],
       ),
     );
